6663bb8ca21f993d6ee752008ed965f2bbce6424
iamquang95 <iamquang95@gmail.com>
2018-06-10 18:04:38 +0700

    Add percent column

--- TimeTracker.py
+++ TimeTracker.py
@@ -9,13 +9,14 @@ class TimeTracker:
     def track(self):
         while True:
             self.__instances.append((self.__get_current_app_name(), int(time.time())))
-            self.__pretty_format_app_list(self.__create_app_list())
+            (app_list, total_time) = self.__create_app_list()
+            self.__pretty_format_app_list(app_list, total_time)
             time.sleep(self.__interval)
 
     # Private methods
     def __init__(self, interval = 1):
         self.__instances = []
-        self.__interval = 1
+        self.__interval = interval
     
     def __get_current_app_name(self):
         current_app = NSWorkspace.sharedWorkspace().activeApplication()
@@ -23,30 +24,39 @@ class TimeTracker:
 
     def __create_app_list(self):
         if not self.__instances:
-            return {}
+            return ({}, 0)
         else:
             app_list = {}
-            cur_time = self.__instances[0][1]
+            cur_time = self.__instances[0][1] - self.__interval
+            total_time = 0
             for (app, time) in self.__instances:
+                time_delta = time - cur_time
                 if (app in app_list):
-                    app_list[app] += (time - cur_time)
+                    app_list[app] += time_delta
                 else:
-                    app_list[app] = 0
+                    app_list[app] = time_delta
+                total_time += time_delta
                 cur_time = time
-            return app_list
+            return (app_list, total_time)
 
-    def __pretty_format_app_list(self, app_list):
+    def __pretty_format_app_list(self, app_list, total_time):
         os.system('cls||clear')
         sorted_app_list = sorted(app_list.items(), key = lambda x: x[1], reverse = True)
-        row_print_format = "|%2s|%20s|%15s|"
-        sperate_row = '+' + '--' + '+' + '-'*20 + '+' + '-'*15 + '+'
+        row_print_format = "|%2s|%20s|%15s|%8s|"
+        sperate_row = '+' + '--' + '+' + '-'*20 + '+' + '-'*15 + '+' + '-'*8 + '+'
 
         print("Application time tracking")
         print(sperate_row)
-        print(row_print_format % ("#", "Application", "Using time"))
+        print(row_print_format % ("#", "Application", "Using time", "Percent"))
         print(sperate_row)
         for idx, (app, time) in enumerate(sorted_app_list):
-            print(row_print_format % (idx+1, app, str(datetime.timedelta(seconds = time))))
+            using_percent = "%.1f" % (time/total_time*100)
+            print(row_print_format % (
+                idx+1,
+                app,
+                str(datetime.timedelta(seconds = time)),
+                using_percent
+                ))
         print(sperate_row)
 
 
